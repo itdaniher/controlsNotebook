36232ef990317bafdefab8bb904e14c9ae52c4f0
Ian Daniher <it.daniher@gmail.com>
2013-03-15 16:45:46 -0400

    fixed a huge number of kludges, broke lagCompensate, greatly improved the appearance of the code

--- compensators.py
+++ compensators.py
@@ -3,16 +3,29 @@
 import sympy
 from sympy.abc import s
 from pylab import *
-from scipy.signal import *
-
+import numpy
+import scipy.signal as signal
 j = sympy.I
 
-ct = 10000
+ct = 100
+
+decibels = lambda lin: 20*numpy.log10(norm(lin))
 
 def eeval(expression, w):
 	""" evaluate a sympy expression at omega. return magnitude, phase."""
 	e = expression.subs(s, j*w)
 	return float(sympy.Abs(e)), float(sympy.arg(e)/sympy.pi*180)
+
+def bode(expression, n = 10):
+	freqs = signal.findfreqs(e2sys(expression).num, e2sys(expression).den, n)
+	magnitude = numpy.array([])
+	phase = numpy.array([])
+	for freq in freqs:
+		(m, p) = eeval(expression, freq)
+		magnitude = numpy.append(magnitude, m)
+		phase = numpy.append(phase, p)
+	magnitude = array(map(decibels, magnitude))
+	return freqs, magnitude, phase
 
 def e2sys(expression):
 	""" basic helper function that accepts a sympy expression, expands it, 
@@ -23,7 +36,7 @@ def e2sys(expression):
 	d = sympy.Poly(sympy.denom(expression), s).all_coeffs()
 	n = map(float, n)
 	d = map(float, d)
-	return lti(n, d)
+	return signal.lti(n, d)
 
 def sys2e(system):
 	""" basic helper function that accepts an instance of the scipy.signal.lti class
@@ -32,17 +45,17 @@ def sys2e(system):
 	num = sum([x*s**y for y,x in enumerate(system.num[::-1])])
 	return sympy.factor(num/den)
 
-def phaseMargin(system):
-	w, mag, phase = bode(system, n=ct)
+def phaseMargin(expression):
+	w, mag, phase = bode(expression, n=ct)
 	crossingPoint = argmin(abs(mag)) # point where magnitude is closest to 0dB
-	return {"w_c": w[crossingPoint], "p_m": 180+phase[crossingPoint]}
+	return {"w_c": w[crossingPoint], "p_m": phase[crossingPoint]+180}
 
-def steadyStepError(system):
-	fvt = eeval(sys2e(system), 0)[0]
+def steadyStepError(expression):
+	fvt = eeval(expression, 0)[0]
 	return {"sse": 1/(1+fvt)}
 
-def reducedGainCompensate(system, target):
-	w, mag, phase = bode(system, n=ct)
+def reducedGainCompensate(expression, target):
+	w, mag, phase = bode(expression, n=ct)
 	phase = [i if i < 0 else -(360-i) for i in phase]
 	phase = array(phase)
 	phaseTarget = (-180 + target)
@@ -51,8 +64,8 @@ def reducedGainCompensate(system, target):
 	magnitude = 10**(magnitude/20)
 	return {"K": 1/magnitude}, 1/magnitude
 
-def dominatePoleCompensate(system, target):
-	w, mag, phase = bode(e2sys(sys2e(system)*1/s), n=ct)
+def dominatePoleCompensate(expression, target):
+	w, mag, phase = bode(expression*1/s, n=ct)
 	phase = [i if i < 0 else -(360-i) for i in phase]
 	phase = array(phase)
 	phaseTarget = (-180 + target)
@@ -61,23 +74,23 @@ def dominatePoleCompensate(system, target):
 	magnitude = 10**(magnitude/20)
 	return {"K": 1/magnitude}, (1/magnitude)*1/s
 
-def lagCompensate(system, target):
-	w, mag, phase = bode(system, n=ct)
-	phase = [i if i < 0 else -(360-i) for i in phase]
-	phase = array(phase)
+def lagCompensate(expression, target):
+	w, mag, phase = bode(expression, n=ct)
 	phaseTarget = (-180 + (target - 6)) # find location of new crossing goal; extra '-6' is from 10/wc rule
 	phaseTargetLocation = argmin(abs(phaseTarget - phase)) 
+	print phase
+	print phaseTargetLocation, phaseTarget, phase[phaseTargetLocation]
 	tau = 10/w[phaseTargetLocation]
-	for alpha in logspace(-2, 5, ct): 
+	for alpha in numpy.logspace(-2, 5, ct): 
 		# equation for basic lag compensator
 		G_c = (tau*s+1)/(alpha*tau*s+1)
-		if abs(phaseMargin(e2sys(G_p*G_c))['p_m']-target) < 0.1:
+		if abs(phaseMargin(expression*G_c)['p_m']-target) < 0.1:
 			break
 	return {"alpha": alpha, "tau": tau}, G_c
 
-def leadCompensate(system, target):
+def leadCompensate(expression, target):
 	# alpha = 10 for 55 degrees phase margin
-	w, mag, phase = bode(system, n=ct)
+	w, mag, phase = bode(expression, n=ct)
 	phase = array([i if i < 0 else -(360-i) for i in phase])
 	alpha = 10
 	phaseTarget = -(180 + (55 - target))
@@ -87,24 +100,22 @@ def leadCompensate(system, target):
 	K_l = 1
 	G_c = K_l * (alpha*tau*s+1)/(tau*s+1)
 	# get magnitude of loop transfer function L(s) at w_c
-	K_l = 1/ eeval(G_c*sys2e(system), w_c)[0] 
+	K_l = 1/ eeval(G_c*expression, w_c)[0] 
 	# equation for basic lead compensator
 	G_c = K_l * (alpha*tau*s+1)/(tau*s+1)
 	return {"K_l": K_l, "alpha": alpha, "tau":tau}, G_c
 
-def characterize(system, f=figure(), color="k", labeled=""):
+def drawBode(expression, f=figure(), color="k", labeled=""):
 	""" draw bode plot for a given scipy.signal.lti instance. """
 	adjustprops = dict(left=0.1, bottom=0.1, right=0.97, top=0.93, hspace=0.2)
 	f.subplots_adjust(**adjustprops)
 	magPlot = f.add_subplot(2, 1, 1)
 	phasePlot = f.add_subplot(2, 1, 2, sharex=magPlot)
-	w, mag, phase = bode(system)
+	w, mag, phase = bode(expression)
 	phase = [i if i < 0 else -(360-i) for i in phase]
-	tImpulse, youtImpulse = impulse2(system)
-	tStep, youtStep = step2(system)
 	magPlot.semilogx(w, mag, label=labeled, color=color)
 	magPlot.set_title("magnitude")
-	magPlot.set_ylabel("amplitude")
+	magPlot.set_ylabel("amplitude (dB)")
 	phasePlot.semilogx(w, phase, label=labeled, color=color)
 	phasePlot.set_title("phase")
 	phasePlot.set_xlabel("radians per second")
@@ -115,19 +126,20 @@ def characterize(system, f=figure(), color="k", labeled=""):
 if __name__ == "__main__":
 	print "before :"
 	G_p = 100/((s+1)*(0.1*s+1)*(0.01*s+1))
-	print phaseMargin(e2sys(G_p))
-	print steadyStepError(e2sys(G_p))
-	characterize(e2sys(G_p), color="k", labeled="uncompensated")
+	G_p = 10/(s*(s+1)*(0.1*s+1))
+	print phaseMargin(G_p)
+	print steadyStepError(G_p)
+	drawBode(G_p, color="k", labeled="uncompensated")
 	print "leadCompensate"
-	(coeffs, G_c) = leadCompensate(e2sys(G_p), 45)
+	(coeffs, G_c) = leadCompensate(G_p, 45)
 	print coeffs
-	print phaseMargin(e2sys(G_p*G_c))
-	print steadyStepError(e2sys(G_p*G_c))
-	characterize(e2sys(G_p*G_c), color="b", labeled="lead")
+	print phaseMargin(G_p*G_c)
+	print steadyStepError(G_p*G_c)
+	drawBode(G_p*G_c, color="b", labeled="lead")
 	print "lagCompensate"
-	(coeffs, G_c) = lagCompensate(e2sys(G_p), 45)
+	(coeffs, G_c) = lagCompensate(G_p, 45)
 	print coeffs
-	print phaseMargin(e2sys(G_p*G_c))
-	print steadyStepError(e2sys(G_p*G_c))
-	characterize(e2sys(G_p*G_c), color="b", labeled="lag")
+	print phaseMargin(G_p*G_c)
+	print steadyStepError(G_p*G_c)
+	drawBode(G_p*G_c, color="r", labeled="lag")
 	show()
