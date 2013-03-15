0a4d9757d8854c513d997746e90694ffb98d0335
Ian Daniher <it.daniher@gmail.com>
2013-03-15 18:49:58 -0400

    cleaned compensators code, now with working lag and lead implementations

--- compensators.py
+++ compensators.py
@@ -7,22 +7,27 @@ import numpy
 import scipy.signal as signal
 j = sympy.I
 
-ct = 100
+ct = 1000
 
 decibels = lambda lin: 20*numpy.log10(norm(lin))
 
 def eeval(expression, w):
 	""" evaluate a sympy expression at omega. return magnitude, phase."""
-	e = expression.subs(s, j*w)
-	return float(sympy.Abs(e)), float(sympy.arg(e)/sympy.pi*180)
+	num, den = e2sys(expression)
+	y = numpy.polyval(num, 1j*w) / numpy.polyval(den, 1j*w)
+	phase = numpy.arctan2(y.imag, y.real) * 180.0 / numpy.pi
+	mag = abs(y)
+	return mag, phase
 
 def bode(expression, n = 10):
-	freqs = signal.findfreqs(e2sys(expression).num, e2sys(expression).den, n)
+	freqs = signal.findfreqs(e2sys(expression)[0], e2sys(expression)[1], n)
 	magnitude = numpy.array([])
 	phase = numpy.array([])
 	for freq in freqs:
 		(m, p) = eeval(expression, freq)
 		magnitude = numpy.append(magnitude, m)
+		if p >= 0:
+			p = -360+p
 		phase = numpy.append(phase, p)
 	magnitude = array(map(decibels, magnitude))
 	return freqs, magnitude, phase
@@ -36,7 +41,7 @@ def e2sys(expression):
 	d = sympy.Poly(sympy.denom(expression), s).all_coeffs()
 	n = map(float, n)
 	d = map(float, d)
-	return signal.lti(n, d)
+	return (n, d)
 
 def sys2e(system):
 	""" basic helper function that accepts an instance of the scipy.signal.lti class
@@ -51,13 +56,12 @@ def phaseMargin(expression):
 	return {"w_c": w[crossingPoint], "p_m": phase[crossingPoint]+180}
 
 def steadyStepError(expression):
-	fvt = eeval(expression, 0)[0]
+	e = expression.subs(s, j*0)
+	fvt = float(sympy.Abs(e))
 	return {"sse": 1/(1+fvt)}
 
 def reducedGainCompensate(expression, target):
 	w, mag, phase = bode(expression, n=ct)
-	phase = [i if i < 0 else -(360-i) for i in phase]
-	phase = array(phase)
 	phaseTarget = (-180 + target)
 	targetIndex = argmin(abs(phaseTarget - phase))
 	magnitude = mag[targetIndex]
@@ -66,8 +70,6 @@ def reducedGainCompensate(expression, target):
 
 def dominatePoleCompensate(expression, target):
 	w, mag, phase = bode(expression*1/s, n=ct)
-	phase = [i if i < 0 else -(360-i) for i in phase]
-	phase = array(phase)
 	phaseTarget = (-180 + target)
 	targetIndex = argmin(abs(phaseTarget - phase))
 	magnitude = mag[targetIndex]
@@ -76,22 +78,19 @@ def dominatePoleCompensate(expression, target):
 
 def lagCompensate(expression, target):
 	w, mag, phase = bode(expression, n=ct)
-	phaseTarget = (-180 + (target - 6)) # find location of new crossing goal; extra '-6' is from 10/wc rule
-	phaseTargetLocation = argmin(abs(phaseTarget - phase)) 
-	print phase
-	print phaseTargetLocation, phaseTarget, phase[phaseTargetLocation]
-	tau = 10/w[phaseTargetLocation]
-	for alpha in numpy.logspace(-2, 5, ct): 
-		# equation for basic lag compensator
-		G_c = (tau*s+1)/(alpha*tau*s+1)
-		if abs(phaseMargin(expression*G_c)['p_m']-target) < 0.1:
-			break
+	phaseTarget = -180 + (target + 6) # find location of new crossing goal; extra '-6' is from 10/wc rule
+	targetIndex = argmin(abs(phaseTarget - phase))
+	print phaseTarget, phase[targetIndex]
+	w_c = w[targetIndex]
+	tau = 10/w_c
+	# equation for basic lag compensator
+	alpha = eeval(expression, w_c)[0]
+	G_c = (tau*s+1)/(alpha*tau*s+1)
 	return {"alpha": alpha, "tau": tau}, G_c
 
 def leadCompensate(expression, target):
 	# alpha = 10 for 55 degrees phase margin
 	w, mag, phase = bode(expression, n=ct)
-	phase = array([i if i < 0 else -(360-i) for i in phase])
 	alpha = 10
 	phaseTarget = -(180 + (55 - target))
 	targetIndex = argmin(abs(phase-phaseTarget))
@@ -125,19 +124,19 @@ def drawBode(expression, f=figure(), color="k", labeled=""):
 
 if __name__ == "__main__":
 	print "before :"
-	G_p = 100/((s+1)*(0.1*s+1)*(0.01*s+1))
+	#G_p = 100/((s+1)*(0.1*s+1)*(0.01*s+1))
 	G_p = 10/(s*(s+1)*(0.1*s+1))
 	print phaseMargin(G_p)
 	print steadyStepError(G_p)
 	drawBode(G_p, color="k", labeled="uncompensated")
 	print "leadCompensate"
-	(coeffs, G_c) = leadCompensate(G_p, 45)
+	(coeffs, G_c) = leadCompensate(G_p, 50)
 	print coeffs
 	print phaseMargin(G_p*G_c)
 	print steadyStepError(G_p*G_c)
 	drawBode(G_p*G_c, color="b", labeled="lead")
 	print "lagCompensate"
-	(coeffs, G_c) = lagCompensate(G_p, 45)
+	(coeffs, G_c) = lagCompensate(G_p, 50)
 	print coeffs
 	print phaseMargin(G_p*G_c)
 	print steadyStepError(G_p*G_c)
